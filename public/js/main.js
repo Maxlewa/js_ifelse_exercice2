41e20d01e023a56e55b73dba4b9bb393b9742cc8
Maxence <lemairemaxence@hotmail.com>
2021-01-05 16:56:46 +0100

    exo in progress

---
+++ public/js/main.js
@@ -0,0 +1,28 @@
+// EXO 1
+
+// let varUn = parseInt(prompt("Nombre 1 :"))
+// let varDeux = parseInt(prompt("Nombre 2 :"))
+
+// if (varUn == varDeux) {
+//     alert(`Les nombres ${varUn} et ${varDeux} sont-ils égaux ? OUI`)
+// }
+
+// else {
+//     alert(`Les nombres ${varUn} et ${varDeux} sont-ils égaux ? NON`)
+// }
+
+// EXO 2
+
+// let varUn = parseInt(prompt("Nombre 1 :"))
+// let varDeux = parseInt(prompt("Nombre 2 :"))
+
+// if (varUn < varDeux) {
+//     alert(`Le nombre ${varUn} est-il plus petit que ${varDeux} ? OUI`)
+// }
+
+// else if (varUn >= varDeux) {
+//     alert(`Le nombre ${varUn} est-il plus petit que ${varDeux} ? NON`)
+// }
+
+// EXO 3
+
