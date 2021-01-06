3673d87569fccc1ab364edfad2a01d937e4913a5
Maxence <lemairemaxence@hotmail.com>
2021-01-06 10:21:22 +0100

    exo 3 et 4 done

--- public/js/main.js
+++ public/js/main.js
@@ -26,3 +26,29 @@
 
 // EXO 3
 
+// let nb1 = parseInt(prompt("Number one ?"))
+// let nb2 = parseInt(prompt("Number two ?"))
+// let nb3 = parseInt(prompt("Number three ?"))
+
+// if (nb1 + nb2 > nb3) {
+//     alert(`La somme de ${nb1} + ${nb2} est-elle plus grande que ${nb3} ? : ${nb1 + nb2 > nb3}`)
+// }
+
+// else if (nb1 + nb2 < nb3) {
+//     alert(`La somme de ${nb1} + ${nb2} est-elle plus grande que ${nb3} ? : ${nb1 + nb2 > nb3}`)
+// }
+
+// EXO 4
+
+let phrase = prompt("Votre phrase :")
+let estimation = parseInt(prompt("Nombre de caractères dans votre phrase à votre avis ?"))
+
+if (phrase.length == estimation) {
+    alert(`Correct ! Votre phrase fait bien ${phrase.length} caractères`)
+}
+
+else {
+    alert(`Faux, votre phrase faisait ${phrase.length} caractères et vous étiez à ${Math.abs(estimation - phrase.length)} caractères de la bonne réponse`)
+}
+
+// EXO 5
